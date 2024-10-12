22258a65c13645640ae1d3dd71461e6f0158b927
Pedro Alves <pedrosabiodos6caminhos@gmail.com>
2024-10-12 12:52:59 -0300

    comit

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,19 @@
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdando da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // O construtor da classe Iphone chama o construtor da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // Implementando o método abstrato "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("12351", modelo: "Mkio", imei: "546k", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("App 01");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("+5581599", modelo: "inoei", imei: "jo54", memoria: 1233);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
+
+
+
